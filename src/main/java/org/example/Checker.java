ae24fb862606035411565937124b644c95154e33
witchkaa <victoriasabadan1@gmail.com>
2024-03-06 19:27:46 +0200

    Complete code for converting
    Provide checks and interpreting for input markdown file

---
+++ src/main/java/org/example/Checker.java
@@ -0,0 +1,76 @@
+package org.example;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Checker {
+    public boolean checkForUnbalancedMarkers(String marker, String blueprint) {
+        int firstOcc = -1;
+        int secondOcc = -1;
+
+        for (int i = 0; i < blueprint.length(); i++) {
+            if (isMarkerAtIndex(marker, blueprint, i)) {
+                boolean atWordEnd = isAtWordEnd(blueprint, i, marker.length());
+                boolean beforeIsMatch = isBeforeMatch(blueprint, i, marker);
+                boolean afterIsMatch = isAfterMatch(blueprint, i, marker);
+                if (isInvalidMarkerPosition(atWordEnd, beforeIsMatch, afterIsMatch)) {
+                    continue;
+                }
+                if (isOpenPosition(firstOcc, secondOcc)) {
+                    firstOcc = i;
+                } else {
+                    secondOcc = i;
+                }
+            }
+            if (isOpenNotClosed(firstOcc, secondOcc, blueprint, i)) {
+                return true;
+            }
+        }
+        return isOpenNotClosed(firstOcc, secondOcc, blueprint, blueprint.length() - 1);
+    }
+
+    private boolean isMarkerAtIndex(String marker, String blueprint, int idx) {
+        return blueprint.startsWith(marker, idx);
+    }
+
+    private boolean isAtWordEnd(String blueprint, int idx, int markerLength) {
+        return (idx > 0 && Character.isLetterOrDigit(blueprint.charAt(idx - 1))) &&
+                (idx + markerLength == blueprint.length() || !Character.isLetterOrDigit(blueprint.charAt(idx + markerLength)));
+    }
+
+    private boolean isBeforeMatch(String blueprint, int idx, String marker) {
+        String regex = "[A-Za-z0-9,\\u0400-\\u04FF]";
+        return idx > 0 && Character.toString(blueprint.charAt(idx - 1)).matches(regex);
+    }
+
+    private boolean isAfterMatch(String blueprint, int idx, String marker) {
+        String regex = "[A-Za-z0-9,\\u0400-\\u04FF]";
+        return idx + marker.length() < blueprint.length() && Character.toString(blueprint.charAt(idx + marker.length())).matches(regex);
+    }
+
+    private boolean isInvalidMarkerPosition(boolean atWordEnd, boolean beforeIsMatch, boolean afterIsMatch) {
+        return atWordEnd || ((!beforeIsMatch && !afterIsMatch) || (beforeIsMatch && afterIsMatch));
+    }
+
+    private boolean isOpenPosition(int openPos, int closePos) {
+        return openPos == -1 || closePos != -1;
+    }
+
+    private boolean isOpenNotClosed(int openPos, int closePos, String blueprint, int idx) {
+        return openPos != -1 && closePos == -1 && (blueprint.charAt(idx) == '\n' || idx == blueprint.length() - 1);
+    }
+    void checkForNestedMarkers(String firstMarker, String secondMarker, List<String> blocks){
+        Pattern firstMarkerPattern = Pattern.compile(firstMarker, Pattern.DOTALL);
+        Pattern secondMarkerPattern = Pattern.compile(secondMarker, Pattern.DOTALL);
+        for (String b : blocks) {
+            Matcher firstMarkerMatcher = firstMarkerPattern.matcher(b);
+            Matcher secondMarkerMatcher = secondMarkerPattern.matcher(b);
+            boolean isFirstMatch = firstMarkerMatcher.find();
+            boolean isSecondMatch = secondMarkerMatcher.find();
+            if (isFirstMatch || isSecondMatch) {
+                throw new Error("Error: invalid markdown (nested tags not allowed). Review your input file and try again.");
+            }
+        }
+    }
+}
--- src/main/java/org/example/Interpreter.java
+++ src/main/java/org/example/Interpreter.java
@@ -3,11 +3,15 @@ package org.example;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Interpreter {
     public String convert(String sourceFile) {
         String md = readMarkdownFile(sourceFile);
-        return convertMarkdownToHtml(md);
+        return convertMdToHtml(md);
     }
     private String readMarkdownFile(String filename) {
         StringBuilder content = new StringBuilder();
@@ -21,12 +25,70 @@ public class Interpreter {
         }
         return content.toString();
     }
+    public String convertMdToHtml(String markdownContent) {
+        List<String> mdBlocks = new ArrayList<>();
 
-    private String convertMarkdownToHtml(String markdownContent) {
-        String htmlContent = markdownContent.replaceAll("\\*\\*(.*?)\\*\\*", "<b>$1</b>");
+        Pattern pattern = Pattern.compile("(?m)(^\\n?|^)```(.*?)```(\\n?|$)", Pattern.DOTALL);
+        Matcher matcher = pattern.matcher(markdownContent);
+        StringBuilder stringBuilder = new StringBuilder();
+        int index = 0;
+        while (matcher.find()) {
+            String prefix = matcher.group(1);
+            String suffix = matcher.group(3);
+            mdBlocks.add(matcher.group(2));
+            String replacement = (prefix.isEmpty() ? "" : "\n") + "block" + index++ + (suffix.isEmpty() ? "" : "\n");
+            matcher.appendReplacement(stringBuilder, Matcher.quoteReplacement(replacement));
+        }
+        matcher.appendTail(stringBuilder);
+        markdownContent = stringBuilder.toString();
+        String blueprint = markdownContent;
 
-        //TODO: Add other regex patterns
+        String findBold = "(?<![\\w`*\u0400-\u04FF])\\*\\*(\\S(?:.*?\\S)?)\\*\\*(?![\\w`*\u0400-\u04FF])";
+        String findItalic = "(?<![\\w`*\\u0400-\\u04FF])_(\\S(?:.*?\\S)?)_(?![\\w`*\\u0400-\\u04FF])";
+        String findMonosp = "(?<![\\w`*\\u0400-\\u04FF])`(\\S(?:.*?\\S)?)`(?![\\w`*\\u0400-\\u04FF])";
 
-        return htmlContent;
+        List<String> boldBlocks = getMatchPatternList(findBold, markdownContent);
+        List<String> italicBlocks = getMatchPatternList(findItalic, markdownContent);
+        List<String> monospacedBlocks = getMatchPatternList(findMonosp, markdownContent);
+
+        Checker checker = new Checker();
+        checker.checkForNestedMarkers(findItalic, findMonosp, boldBlocks);
+        checker.checkForNestedMarkers(findBold, findItalic, monospacedBlocks);
+        checker.checkForNestedMarkers(findBold, findMonosp, italicBlocks);
+
+        markdownContent = markdownContent.replaceAll(findBold, "<b>$1</b>");
+        blueprint = blueprint.replaceAll(findBold, "boldBlock");
+        markdownContent = markdownContent.replaceAll(findItalic, "<i>$1</i>");
+        blueprint = blueprint.replaceAll(findItalic, "italicBlock");
+        markdownContent = markdownContent.replaceAll(findMonosp, "<tt>$1</tt>");
+        blueprint = blueprint.replaceAll(findMonosp, "monospacedBlock");
+
+        String[] paragraphs = markdownContent.split("\n{2,}");
+        StringBuilder builder = new StringBuilder();
+        for (String paragraph : paragraphs) {
+            if (!paragraph.isEmpty()) {
+                builder.append("<p>").append(paragraph).append("</p>\n");
+            }
+        }
+        markdownContent = builder.toString();
+        for (int i = 0; i < mdBlocks.size(); i++) {
+            markdownContent = markdownContent.replace("block" + i, "<pre>" + mdBlocks.get(i) + "</pre>");
+        }
+        if (checker.checkForUnbalancedMarkers("`", blueprint) ||
+                checker.checkForUnbalancedMarkers("```", blueprint) ||
+                checker.checkForUnbalancedMarkers("**", blueprint) ||
+                checker.checkForUnbalancedMarkers("_", blueprint)) {
+            throw new Error("Error: invalid markdown (some markup element was not closed). Review your input file and try again.");
+        }
+        return markdownContent;
+    }
+    private List<String> getMatchPatternList(String regex, String html) {
+        List<String> regexBlocks = new ArrayList<>();
+        Pattern regexPatten = Pattern.compile(regex, Pattern.DOTALL);
+        Matcher boldMatcher = regexPatten.matcher(html);
+        while (boldMatcher.find()) {
+            regexBlocks.add(boldMatcher.group(1));
+        }
+        return regexBlocks;
     }
 }
