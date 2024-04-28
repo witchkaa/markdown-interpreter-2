dd8e13ed7e79897426d20fe3826aa8cb4dd95741
witchkaa <victoriasabadan1@gmail.com>
2024-04-28 20:49:15 +0300

    Refactor and add tests for Main

--- src/main/test/MainTest.java
+++ src/main/test/MainTest.java
@@ -1,2 +1,48 @@
-public class MainTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.PrintStream;
+import org.example.Main;
+
+class MainTest {
+    @Test
+    void testConvertMarkdownToHtmlMissingSourceFile() {
+        String[] args = {};
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setErr(new PrintStream(outContent));
+
+        Main.main(args);
+
+        String printedOutput = outContent.toString().trim();
+
+        assertEquals("Please provide the path to source file.", printedOutput);
+    }
+    @Test
+    void testConvertMarkdownToHtmlOK() {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        String sourceFilePath = "src/main/test/file.md";
+        String[] args = {sourceFilePath};
+
+        Main.main(args);
+        String consoleOutput = outContent.toString().trim();
+        assertEquals("<p>this file was created for <b>testing</b> purposes</p>", consoleOutput);
+    }
+    @Test
+    void testConvertMarkdownToHtmlOutputFileOption() {
+        String sourceFilePath = "src/main/test/file.md";
+        String outputFilePath = "src/main/test/file.html";
+        String[] args = {sourceFilePath, "-o", outputFilePath};
+
+        Main.main(args);
+
+        File outputFile = new File(outputFilePath);
+        assertTrue(outputFile.exists(), "Output file should be created");
+        assertTrue(outputFile.isFile(), "Output should be a file");
+        assertTrue(outputFile.length() > 0, "Output file should not be empty");
+
+        outputFile.delete();
+    }
 }
